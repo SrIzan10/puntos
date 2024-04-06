6add0a5c48f64de8512706ca3ce5429949c357e3
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2024-04-06 17:28:58 +0200

    chore: remove api route

--- src/app/api/verifyAuth/route.ts
+++
@@ -1,8 +0,0 @@
-import { validateRequest } from "@/lib/auth";
-
-export default async function GET(request: Request) {
-    const { user } = await validateRequest();
-    if (!user) {
-        return Response.redirect("/auth/login");
-    }
-}
